926213ab7604ad378f9951acb27154b13b4823e7
bytecodesky <skynetoffc@gmail.com>
2024-07-23 01:14:52 -0500

    CourseInfo exercises

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,62 @@
+import './App.css';
+
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  );
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  );
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part[0].name} exercises={props.part[0].exercises} />
+      <Part part={props.part[1].name} exercises={props.part[1].exercises} />
+      <Part part={props.part[2].name} exercises={props.part[2].exercises} />
+    </div>
+  );
+}
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises {props.part[0].exercises + props.part[1].exercises + props.part[2].exercises}
+    </p>
+  );
+}
+
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ];
+
+  return (
+    <div className='content'>
+      <Header course={course} />
+      <Content part={parts} />
+      <Total part={parts} />
+    </div>
+  )
+}
+
+export default App
